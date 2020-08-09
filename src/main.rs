f2db0a5fe9c9ce1159c8af709949ebb5f1dfde25
Grzegorz Szeliga <grzegorzszeliga@macbook-pro-grzegorz.home>
2020-08-09 23:58:32 +0200

    Subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,5 @@
 #![allow(non_snake_case)]
 
-//https://crates.io/crates/simple-mutex
-
-
-// struct Context {
-// }
-
-// impl Context {
-//     pub fn new<T>() -> Value<T> {
-//         todo!();
-//     }
-
-//     pub fn calculate<K>(fun: Box<dyn Fn() -> K>) -> Computed<K> {
-//         todo!();
-//     }
-// }
-
-// struct Value<T> {
-//     value: T,
-// }
-
-// impl<T> Value<T> {
-//     pub fn toComputed(&self) -> Computed<T> {
-//         todo!();
-//     }
-// }
-
-// struct Computed<T> {
-//     get: Box<dyn Fn() -> T>,
-// }
-
-// impl<T> Computed<T> {
-// }
-
-
-/*
-                                fn() ->             tylko czyste funkcje
-
-Value --> T
-Value --> K
-
-fn(
-    fn() -> T,          //subskrybcja podczas obliczania
-    fn() -> K
-) -> R
-
-
-
-Value =====>    impl ValueTrait
-
-    get() -> T                                      === pobiera oraz subskrybuje
-
-
-
-let context: Context = Context::new();
-
-let a: Value<u64> = context.value(3);
-let b: Value<u32> = context.value(5);
-
-
-lub bardziej mobxowo
-Przewaga nad jsem nawet w tej wersji jest taka, że nie da się zrobić cykliczności pomiędzy zmiennymi
-
-let c: Computed<u64> = context.combine2(Box::new(move fn(&mut context) -> u64 {
-
-    let aValue = context.get(a);    //,.get();
-    let bValue = context.get(b);    //.get();
-
-    todo!();
-}));
-
-
-context.subscribe(c, fn(value: u64) -> {
-    println!("wynik: {}", value);
-});
-
-context.set(a, 4);
-context.set(b, 55);
-
-
-Component
-    render() {
-
-    }
-
-    Component
-        render() {
-
-        }
-
-*/
-
 use std::rc::Rc;
 use std::cell::RefCell;
 
@@ -142,7 +51,7 @@ impl<T: 'static> Value<T> {
         })
     }
 
-    pub fn setValue(&mut self, value: T) -> Vec<Box<dyn Subscriber>> {
+    pub fn setValue(&mut self, value: T) -> Vec<Box<dyn Subscriber>> {                          //TODO - trzeba odebrać i wywołać
         self.value = Rc::new(value);
         self.subscription.trigger()
     }
@@ -213,6 +122,9 @@ impl<T: 'static> Computed<T> {
         b: Rc<Computed<B>>,
         calculate: fn(Rc<A>, Rc<B>) -> R
     ) -> Rc<Computed<R>> {
+
+        //TODO - dodać subskrybcje ...
+
         let getValue = Box::new(move || {
             let aValue = a.getValue();
             let bValue = b.getValue();
@@ -239,11 +151,43 @@ impl<T: 'static> Computed<T> {
         inner.isFresh = false;
         inner.subscription.trigger()
     }
+
+    pub fn subscribe<F: Fn(T)>(self: Rc<Self>, fun: F) {
+
+        todo!();
+    }
 }
 
 impl<T> Observer for Rc<Computed<T>> {
     fn call(&self) -> Vec<Box<dyn Subscriber>> {
         self.setAsUnfresh()
+    }
+}
+
+impl<T> Drop for Computed<T> {
+    fn drop(&mut self) {
+
+        //TODO - odsybskrybować
+        todo!();
+    }
+}
+
+struct Client<T: 'static> {
+    call: Box<dyn Fn(Rc<T>) + 'static>,
+}
+
+impl<T> Client<T> {
+    fn new(call: Box<dyn Fn(Rc<T>) + 'static>) -> Client<T> {
+        Client {
+            call
+        }
+    }
+}
+impl<T> Drop for Client<T> {
+    fn drop(&mut self) {
+
+        //TODO - odsybskrybować
+        todo!();
     }
 }
 
