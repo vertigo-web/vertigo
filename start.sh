ac083abbf8bf866b8eb452b9df0c143f4ed92c96
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2020-08-26 07:50:35 +0200

    info

--- start.sh
+++ start.sh
@@ -3,6 +3,16 @@
 
 #cargo run
 
+echo ""
+echo ""
+echo ""
+echo "--------------------------------------"
+echo "Startuję serwer http://127.0.0.1:8080/"
+echo "--------------------------------------"
+echo ""
+echo ""
+echo ""
+
 cd app_www
 
 npm run start
