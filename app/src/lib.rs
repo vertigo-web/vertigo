1ff2672d4dc144ac302ad2c82850207b7efab3a3
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2020-09-28 07:38:04 +0200

    start

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,23 +1,45 @@
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 
-mod application;
 mod app_state;
 mod view;
 
 use wasm_bindgen::prelude::*;
 
 use std::cell::RefCell;
-use crate::application::applicationStart;
 
-use virtualdom::vdom::App::App;
+use virtualdom::{
+    computed::{
+        Dependencies::Dependencies,
+    },
+    vdom::{
+        App::App,
+        StateBox::StateBox,
+    }
+};
+
+use browserdriver::{
+    DomDriverBrowser,
+};
+
+use app_state::AppState;
+use view::main_render::main_render;
+
+
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 thread_local! {
-    static APP_STATE: RefCell<App> = RefCell::new(applicationStart());
+    static APP_STATE: RefCell<App> = RefCell::new({
+        let root: Dependencies = Dependencies::new();
+        let appStateBox = StateBox::new(&root, AppState::new(&root));
+    
+        let driver = DomDriverBrowser::new();
+    
+        App::new(driver, appStateBox.render(main_render))
+    });
 }
 
 #[wasm_bindgen(start)]
@@ -29,3 +51,29 @@ pub fn start_app() {
 
     APP_STATE.with(|state| state.borrow().start_app());
 }
+
+
+/*
+TODO - Dodać tranzakcyjną aktualizację
+    self.deps.triggerChange([id1, id2, id3]);               //to powinno wystarczyc
+
+TODO - Graph - usunac nieuzywane krawedzie (subskrybcje)
+
+TODO - Graph - zamienić Clone na Copy
+
+TODO - dodać jakieś makra które pozwolą na łatwe generowanie html-a (https://docs.rs/maplit/1.0.2/maplit/)
+
+TODO - Będąc w bloku computed, albo subskrybcji, całkowicie ignorować wszelkie akcje które będą chciały zmienić wartość
+       rzucać standardowy strumień błędów informację o incydencie. Dzięki temu nowa wadliwa funkcjonalność nie zepsuje tej juz dobrze ulezanej funkcjonalności
+    
+TODO - poprawić mechanizm synchronizowania listy dzieci
+    1. brać listę istniejących juz nodów (bez odczepiania od parenta)
+    2. wygenerowac nowa liste
+    3. wyszukac pierwszy usuniety node, za nim podczapiac te nowe we własciwej kolejnośli
+        lub wyszukac pierwszy który istnieje i za nim podczepiać te kolejne
+
+TODO - niezmienne struktury danych, https://docs.rs/im/15.0.0/im/
+
+https://github.com/rustwasm/console_error_panic_hook#readme
+https://rustwasm.github.io/wasm-bindgen/reference/passing-rust-closures-to-js.html
+*/
