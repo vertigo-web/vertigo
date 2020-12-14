5cb0e02428f49187703274a62d4a3201deb9d7aa
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2020-12-14 23:38:38 +0100

    item cell

--- app/src/sudoku/render/mod.rs
+++ app/src/sudoku/render/mod.rs
@@ -32,9 +32,38 @@ fn CssWrapper() -> Css {
     ", config.allWidth, config.allWidth))
 }
 
+fn CssItemWrapper() -> Css {
+    let config = Config::new();
+    Css::new(format!("
+        border: {}px solid black;
+
+        width: {}px;
+        height: {}px;
+
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        flex-shrink: 0;
+    ", config.groupBorderSize, config.groupWidthSize, config.groupWidthSize))
+}
+
+fn CssCell() -> Css {
+    let config = Config::new();
+    Css::new(format!("
+        border: {}px solid green;
+        width: {}px;
+        height: {}px;
+    ", config.itemBorderSize, config.itemWidthSize, config.itemWidthSize))
+}
+
 fn getGroup(sudoku: &Computed<Sudoku>, x: TreeBoxIndex, y: TreeBoxIndex) -> Computed<SudokuSquare<Cell>> {
     sudoku.clone().map(move |state| {
         state.getValue().grid.getFrom(x, y)
+    })
+}
+
+fn getCell(group: &Computed<SudokuSquare<Cell>>, x: TreeBoxIndex, y: TreeBoxIndex) -> Computed<Cell> {
+    group.clone().map(move |state| {
+        state.getValue().getFrom(x, y)
     })
 }
 
@@ -42,7 +71,25 @@ fn render_group(group: &Computed<SudokuSquare<Cell>>) -> VDomNode {
     use NodeAttr::{buildNode, node, css, text, onClick, component};
 
     buildNode("div", vec!(
-        text("grupa"),
+        css(CssItemWrapper()),
+        component(getCell(group, TreeBoxIndex::First,  TreeBoxIndex::First),  render_cell),
+        component(getCell(group, TreeBoxIndex::First,  TreeBoxIndex::Middle), render_cell),
+        component(getCell(group, TreeBoxIndex::First,  TreeBoxIndex::Last),   render_cell),
+        component(getCell(group, TreeBoxIndex::Middle, TreeBoxIndex::First),  render_cell),
+        component(getCell(group, TreeBoxIndex::Middle, TreeBoxIndex::Middle), render_cell),
+        component(getCell(group, TreeBoxIndex::Middle, TreeBoxIndex::Last),   render_cell),
+        component(getCell(group, TreeBoxIndex::Last,   TreeBoxIndex::First),  render_cell),
+        component(getCell(group, TreeBoxIndex::Last,   TreeBoxIndex::Middle), render_cell),
+        component(getCell(group, TreeBoxIndex::Last,   TreeBoxIndex::Last),   render_cell),
+    ))
+}
+
+fn render_cell(item: &Computed<Cell>) -> VDomNode {
+    use NodeAttr::{buildNode, node, css, text, onClick, component};
+
+    buildNode("div", vec!(
+        css(CssCell()),
+        text("0")                       //TODO
     ))
 }
 
