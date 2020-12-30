70eaad05b306951959a69a0d8054f96d6abb0ec9
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2020-12-30 08:27:16 +0100

    Fixed when the value did not change in computed value, bad subscriptions were retained

--- app/src/sudoku/render/mod.rs
+++ app/src/sudoku/render/mod.rs
@@ -54,7 +54,7 @@ fn css_cell_wrapper() -> Css {
 fn render_cell(item: &Computed<Cell>) -> VDomNode {
     let value = *item.get_value().number.value.get_value();
 
-    //log::warn!("cell {:?}", value);
+    // log::warn!("cell {:?}", value);
     if let Some(value) = value {
         return render_cell_value::render_cell_value(value, item);
     }
