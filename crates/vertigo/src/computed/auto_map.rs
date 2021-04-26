0092936a106ac6812a81a1b1e52989ae919a7ca3
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2021-04-26 08:44:43 +0200

    Added AutoMapBox

--- crates/vertigo/src/computed/auto_map.rs
+++ crates/vertigo/src/computed/auto_map.rs
@@ -56,3 +56,53 @@ impl<K: Eq + Hash + Clone, V: PartialEq + 'static> AutoMap<K, V> {
         new_item
     }
 }
+
+
+
+#[derive(PartialEq, Clone)]
+pub struct AutoMapBox<K: Eq + Hash + Clone, V: PartialEq + Clone + 'static> {
+    create: Rc<EqBox<Box<dyn Fn(&K) -> V>>>,
+    values: Rc<EqBox<BoxRefCell<HashMap<K, V>>>>,
+}
+
+impl<K: Eq + Hash + Clone, V: PartialEq + Clone + 'static> AutoMapBox<K, V> {
+    pub fn new<C: Fn(&K) -> V + 'static>(create: C) -> AutoMapBox<K, V> {
+        AutoMapBox {
+            create: Rc::new(EqBox::new(Box::new(create))),
+            values: Rc::new(EqBox::new(BoxRefCell::new(HashMap::new(), "auto map box values"))),
+        }
+    }
+
+    pub fn get_value(&self, key: &K) -> V {
+        let item: Option<V> = self.values.get_with_context(
+            key,
+            |state, key| -> Option<V> {
+                let item = (*state).get(key);
+
+                if let Some(item) = item {
+                    return Some(item.clone());
+                }
+
+                None
+            }
+        );
+
+        if let Some(item) = item {
+            return item;
+        }
+
+        let new_item = {
+            let create = &self.create;
+            create(key)
+        };
+
+        self.values.change(
+            (key, &new_item),
+            |state, (key, new_item)| {
+                (*state).insert(key.clone(), new_item.clone());
+            }
+        );
+
+        new_item
+    }
+}
