d3c8ff2313be12656c13ab16d98480d5180dfa71
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2021-05-17 07:19:41 +0200

    Added action structures

--- crates/vertigo/src/utils/mod.rs
+++ crates/vertigo/src/utils/mod.rs
@@ -1,7 +1,9 @@
 mod box_ref_cell;
 mod eq_box;
 mod drop_resource;
+mod action;
 
 pub use box_ref_cell::BoxRefCell;
 pub use eq_box::EqBox;
 pub use drop_resource::DropResource;
+pub use action::{Action, ActionSubscribe};
