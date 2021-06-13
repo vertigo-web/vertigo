a2a04ae73e3183ee53faefc835ea738d6e8fab6d
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2021-06-13 16:40:44 +0200

    Simplifying the start of applications

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -2,7 +2,7 @@ use wasm_bindgen::prelude::wasm_bindgen;
 
 use vertigo::{
     computed::Dependencies,
-    App,
+    start_app,
     VDomComponent,
 };
 
@@ -14,7 +14,7 @@ mod app;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen(start)]
-pub async fn start_app() {
+pub async fn start_application() {
     console_error_panic_hook::set_once();
     wasm_logger::init(wasm_logger::Config::default());
 
@@ -24,17 +24,5 @@ pub async fn start_app() {
     let driver = DomDriverBrowser::new(&root);
     let app_state = app::State::new(&root, &driver);
 
-    //for testing
-    // root.set_hook(
-    //     Box::new(|| {
-    //         log::info!("before start");
-    //     }),
-    //     Box::new(|| {
-    //         log::info!("after end");
-    //     })
-    // );
-
-    let app = App::new(driver.clone(), VDomComponent::new(app_state, app::render));
-
-    app.start_app().await;
+    start_app(driver, VDomComponent::new(app_state, app::render)).await;
 }
