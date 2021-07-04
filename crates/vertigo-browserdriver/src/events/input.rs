9967b52fab5567363e453b5b88457c70152fb6a0
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2021-07-04 13:50:58 +0200

    Improved structures in the dom_driver module

--- crates/vertigo-browserdriver/src/events/input.rs
+++ crates/vertigo-browserdriver/src/events/input.rs
@@ -2,29 +2,27 @@ use std::rc::Rc;
 
 use wasm_bindgen::{JsCast};
 
-use vertigo::{RealDomId, utils::BoxRefCell};
+use vertigo::{RealDomId};
 use web_sys::{Element, HtmlInputElement, HtmlTextAreaElement};
 
-use crate::{DomDriverBrowserInner, dom_event::DomEvent};
+use crate::dom_driver_browser::{DomDriverBrowser};
+use crate::dom_event::DomEvent;
 
-use super::{find_dom_id, get_from_node};
 
-
-fn find_event_on_input(inner: &Rc<BoxRefCell<DomDriverBrowserInner>>, id: RealDomId) -> Option<Rc<dyn Fn(String)>> {
-    get_from_node(
-        inner,
+fn find_event_on_input(dom_driver: &DomDriverBrowser, id: RealDomId) -> Option<Rc<dyn Fn(String)>> {
+    dom_driver.get_from_node(
         &id,
         |elem| elem.on_input.clone()
     )
 }
 
-pub fn create_input_event(inner: &Rc<BoxRefCell<DomDriverBrowserInner>>, root: &Element) -> DomEvent {
-    let inner = inner.clone();
+pub fn create_input_event(dom_driver: &DomDriverBrowser, root: &Element) -> DomEvent {
+    let dom_driver = dom_driver.clone();
 
     DomEvent::new(&root, "input", move |event: web_sys::Event| {
 
-        let dom_id = find_dom_id(&event);
-        let event_to_run = find_event_on_input(&inner, dom_id);
+        let dom_id = dom_driver.find_dom_id(&event);
+        let event_to_run = find_event_on_input(&dom_driver, dom_id);
 
         if let Some(event_to_run) = event_to_run {
             let target = event.target().unwrap();
