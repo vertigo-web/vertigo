0b47df082c5f125626e480fbb86a28948a993b9b
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2022-11-24 20:31:03 +0100

    vertig-cli, libraries added: html_query_parser, async-recursion, tokio

--- demo/app/src/app/counters/mod.rs
+++ demo/app/src/app/counters/mod.rs
@@ -72,8 +72,32 @@ impl CountersDemo {
             padding: 1px;
             margin: 0 auto;
             display: block;
+
+            cursor: pointer;
+            box-shadow: 4px 4px 4px #444, 8px 8px 4px #666, 12px 12px 4px #888;
+
+            transition: all .2s ease-in-out;
+            :hover {
+                transform: scale(1.1);
+            }
         "};
 
+        let center_css2 = css!{"
+            border: 1px solid black;
+            padding: 1px;
+            margin: 0 auto;
+            display: block;
+
+            cursor: pointer;
+
+            box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.5), 8px 8px 4px rgba(0, 0, 0, 0.4), 12px 12px 4px rgba(0, 0, 0, 0.3);
+            transition: all .2s ease-in-out;
+            :hover {
+                transform: scale(1.5);
+                box-shadow: 54px 54px 14px rgba(0, 0, 0, 0.3), 58px 58px 14px rgba(0, 0, 0, 0.2), 62px 62px 14px rgba(0, 0, 0, 0.1);
+            }
+        "};
+    
         dom! {
             <div>
                 <SimpleCounter label="counter1 value" value={&self.state.counter1} />
@@ -82,6 +106,7 @@ impl CountersDemo {
                 <SimpleCounter label="counter4 value" value={&self.state.counter4} />
                 <Sum sum={&self.state.sum} />
                 <img css={center_css} src={path} />
+                <img css={center_css2} src={path} />
             </div>
         }
     }
