d316a8998a4a6ecf7c097590a43c755c120f3b24
sfisol <wolfmoon@o2.pl>
2025-02-21 12:26:05 +0100

    Intercept inserting mutiple html/head/body tags in DOM (#297)

--- crates/vertigo/src/dom/dom_id.rs
+++ crates/vertigo/src/dom/dom_id.rs
@@ -1,4 +1,7 @@
-use std::hash::Hash;
+use std::{
+    hash::Hash,
+    sync::atomic::{AtomicBool, Ordering},
+};
 
 const HTML_ID: u64 = 1;
 const HEAD_ID: u64 = 2;
@@ -10,6 +13,11 @@ fn get_unique_id() -> u64 {
     static COUNTER: AtomicU64 = AtomicU64::new(START_ID);
     COUNTER.fetch_add(1, Ordering::Relaxed)
 }
+
+// Flags to ensure we have only 1 instance of these elements with predefined id
+static HAD_HTML: AtomicBool = AtomicBool::new(false);
+static HAD_HEAD: AtomicBool = AtomicBool::new(false);
+static HAD_BODY: AtomicBool = AtomicBool::new(false);
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct DomId(u64);
@@ -23,9 +31,9 @@ impl Default for DomId {
 impl DomId {
     pub fn from_name(name: &str) -> DomId {
         let new_id = match name {
-            "html" => HTML_ID,
-            "head" => HEAD_ID,
-            "body" => BODY_ID,
+            "html" => maybe_static_id("html", &HAD_HTML, HTML_ID),
+            "head" => maybe_static_id("head", &HAD_HEAD, HEAD_ID),
+            "body" => maybe_static_id("body", &HAD_BODY, BODY_ID),
             _ => get_unique_id(),
         };
 
@@ -48,5 +56,15 @@ impl DomId {
 impl std::fmt::Display for DomId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "RealDomNodeId={}", self.0)
+    }
+}
+
+fn maybe_static_id(name: &'static str, flag: &'static AtomicBool, static_id: u64) -> u64 {
+    if flag.load(Ordering::Relaxed) {
+        log::error!("Multiple <{name}> elements!");
+        get_unique_id()
+    } else {
+        flag.store(true, Ordering::Relaxed);
+        static_id
     }
 }
