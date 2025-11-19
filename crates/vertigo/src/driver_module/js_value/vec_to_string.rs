6a171119321e7175e2b454bf9e413702128d3a40
Michał Pokrywka <wolfmoon@o2.pl>
2025-11-19 19:53:17 +0100

    Use base64 for embedding SSRFetchCache in HTML

--- crates/vertigo/src/driver_module/js_value/vec_to_string.rs
+++ crates/vertigo/src/driver_module/js_value/vec_to_string.rs
@@ -1,34 +1,21 @@
+use base64::{engine::general_purpose, Engine};
+
 pub fn vec_to_string(data: &[u8]) -> String {
-    let encoded = data
-        .iter()
-        .map(|b| format!("{:02x} ", b))
-        .collect::<String>();
-    encoded
+    general_purpose::STANDARD_NO_PAD.encode(data)
 }
 
 pub fn string_to_vec(encoded: &str) -> Result<Vec<u8>, String> {
-    let mut result = Vec::with_capacity((encoded.len() / 3) + 1);
-
-    for i in (0..encoded.len()).step_by(3) {
-        match u8::from_str_radix(&encoded[i..i + 2], 16) {
-            Ok(char) => {
-                result.push(char);
-            }
-            Err(_err) => {
-                return Err(format!("decoding error on character index={i}"));
-            }
-        }
-    }
-
-    Ok(result)
+    general_purpose::STANDARD_NO_PAD
+        .decode(encoded)
+        .map_err(|err| format!("ssr cache decoding error: {err}"))
 }
 
 #[test]
 fn test() {
-    let data = vec![44, 55, 66, 70];
+    let data = vec![84, 68, 83, 32, 106];
 
     let str = vec_to_string(&data);
-    assert_eq!(str, "2c 37 42 46 ".to_string());
+    assert_eq!(str, "VERTIGo".to_string());
 
     let data2 = string_to_vec(&str);
 
